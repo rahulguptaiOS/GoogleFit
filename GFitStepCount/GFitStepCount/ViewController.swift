94d78121eb3b96c9bb094d490711eb9079dfe160
Rahul Gupta <rahul.gupta2@cbre.com>
2021-07-30 01:36:41 +0530

    Check

--- GFitStepCount/GFitStepCount/ViewController.swift
+++ GFitStepCount/GFitStepCount/ViewController.swift
@@ -40,7 +40,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func signIn(_ sender: Any) {
